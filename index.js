cb5e81aee478729fdfc72175dafb0545ebcca07f
Dimitri237 <123161517+Dimitri237@users.noreply.github.com>
2024-08-19 14:17:43 +0100

    update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const pool = new Pool({
 const app = express();
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:8080/'
+    origin: 'http://localhost:8080'
 }));
 
 const Papa = require('papaparse');
